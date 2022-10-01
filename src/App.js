c294b23a93612faabe245e29e7b3ebc842260fbf
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-01 12:47:07 +0600

    bootstrap setup and initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" className="btn btn-danger">Danger</button>
+        <button type="button" className="btn btn-warning">Warning</button>
+        <button type="button" className="btn btn-info">Info</button>
+      </div>
+
+      <div className="spinner-border text-info" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
+
   );
 }
 
